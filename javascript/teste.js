4568c01be28cac5faaf9f1f17fc27285208f15c1
Kauan de Oliveira Carvalho <kauan.carvalho@corregedoria.df>
2024-12-20 08:54:04 -0300

    inicializei o teste

---
+++ javascript/teste.js
@@ -0,0 +1,10 @@
+function DolarparaReal(Real, Dolar) {
+    return Real / Dolar
+}
+document.getElementById('Real')
+var Real = parseInt(prompt('Quanto você tem em real?'), 10);
+
+var Dolar = 6.08;
+var Total = Real / Dolar;
+
+alert('O valor que você possui em real é R$: ' + Real + ' e na cotação atual seria: ' + Total.toFixed(2) + ' dólares.');
